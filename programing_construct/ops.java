2c5fd82827743029353468512ad38c15f8493bbf
unknown <lifemindvoice@gmail.com>
2026-01-30 19:33:34 +0530

    Remove programming_construct files

--- programing_construct/ops.java
+++
@@ -1,8 +0,0 @@
-class Operator {
-	public static void main(String ... args){
-		int a = 10;
-		a += 5;
-		System.out.println(a);
-	
-	}
-}
--- programing_construct/una.java
+++
@@ -1,8 +0,0 @@
-class Operator {
-	public static void main(String args[]){
-		int value = 10;
-		System.out.println(value++);
-		System.out.println(++value);
-	
-	}
-}
